75d2f10b3a40f6a54de87c0d1d56893b34afe40e
Petar091 <p.cuckovic89@gmail.com>
2019-10-31 13:07:49 +0100

    Creating home background and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import {FaHome} from 'react-icons/fa'
-  
+
+ 
 
 export default class Navbar extends Component {
     render() {
+
+
+     
+
+
         return (
 
           
@@ -11,20 +17,20 @@ export default class Navbar extends Component {
 
             <nav class="navbar navbar-expand-lg navbar-light bg-dark">
             
-  <a class="navbar-brand text-white text-uppercase" href="#">Car Application</a>
+  <a class="navbar-brand text-white text-uppercase" href="/">Car Application</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav m-auto">
       <li class="nav-item active">
-        <a class="nav-link text-white text-uppercase ml-5" href="#"><FaHome/> Home Page<span class="sr-only">(current)</span></a>
+       <a class="nav-link text-white text-uppercase ml-5" href="/"> <FaHome/> Home Page<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white text-uppercase ml-5" href="#">Vehicle Make</a>
+       <a class="nav-link text-white text-uppercase ml-5" href="/VehicleMake">VehicleMake </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white text-uppercase ml-5" href="#">Vehicle Model</a>
+        <a class="nav-link text-white text-uppercase ml-5" href="/VehicleModel">   Vehicle Model</a>
       </li>
     </ul>
     <span class="navbar-text">
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,32 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import MainImage from '../assets/backgroundImg.jpeg'
+/*import MainImage from '../assets/backgroundImg.jpeg'*/
+
 
 export default function HomePage() {
 
     return (
         
-               
-              <div class="container-fluid px-0">
-                  <div class="row mx-0">
-                      <div class="col-12 px-0">
-                <img src={MainImage} alte="Car background image" class="img-fluid"></img>  
-               <Link to="/VehicleMake">
+              
+              <div class="HomePage">
+                  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
+                      <div class="home-text">
+                <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>  
+             <Link to="/VehicleMake">
                <button type="button" class="btn btn-secondary">Enter Application</button>
                </Link>
-                </div>
                </div>
-            </div>
+               <div class="home-footer-text">
+              <p class="text-muted"> © 2019 Copyright:></p>
+              </div>
+               </div>
+                </div>
+              
+              
      
-
+      
 
        
     );
 }
+

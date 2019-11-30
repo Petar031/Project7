7cde9689add26d6edfee39fddd8cc37f46d7d076
Petar091 <p.cuckovic89@gmail.com>
2019-11-30 19:13:44 +0100

    New tables

--- src/components/TableMake.js
+++ src/components/TableMake.js
@@ -1,131 +1,145 @@
-import React, { Component } from 'react';
+import React from 'react';
+import MaterialTable from 'material-table';
 /*import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';*/
 /*import Vehicles from "./pages/VehicleMake";*/
 /*import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';*/
-import {FaSort} from 'react-icons/fa'
-import { MDBDataTable } from 'mdbreact'; 
+/*import {FaSort} from 'react-icons/fa'*/
+/*import { MDBDataTable } from 'mdbreact'; */
 
 
 
 
 
-
+/*
 export default class TableMake extends Component {
 
  
   
  
-  
- 
- 
-   
 
  render(){
-
-  
+ 
    
 
      return (
 
-      <div class="card">
-      <h3 class="card-header text-center font-weight-bold text-uppercase py-4">Editable table</h3>
-      <div class="card-body">
-        <div id="table" class="table-editable">
-          <span class="table-add float-right mb-3 mr-2"><a href="#!" class="text-success"><i
-                class="fas fa-plus fa-2x" aria-hidden="true"></i></a></span>
-          <table class="table table-bordered table-responsive-md table-striped text-center">
-            <thead>
-              <tr>
-                <th class="text-center">Person Name</th>
-                <th class="text-center">Age</th>
-                <th class="text-center">Company Name</th>
-                <th class="text-center">Country</th>
-                <th class="text-center">City</th>
-                <th class="text-center">Sort</th>
-                <th class="text-center">Remove</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="pt-3-half" contenteditable="true">Aurelia Vega</td>
-                <td class="pt-3-half" contenteditable="true">30</td>
-                <td class="pt-3-half" contenteditable="true">Deepends</td>
-                <td class="pt-3-half" contenteditable="true">Spain</td>
-                <td class="pt-3-half" contenteditable="true">Madrid</td>
-                <td class="pt-3-half">
-                  <span class="table-up"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-up"
-                        aria-hidden="true"></i></a></span>
-                  <span class="table-down"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-down"
-                        aria-hidden="true"></i></a></span>
-                </td>
-                <td>
-                  <span class="table-remove"><button type="button"
-                      class="btn btn-danger btn-rounded btn-sm my-0">Remove</button></span>
-                </td>
-              </tr>
-            
-              <tr>
-                <td class="pt-3-half" contenteditable="true">Guerra Cortez</td>
-                <td class="pt-3-half" contenteditable="true">45</td>
-                <td class="pt-3-half" contenteditable="true">Insectus</td>
-                <td class="pt-3-half" contenteditable="true">USA</td>
-                <td class="pt-3-half" contenteditable="true">San Francisco</td>
-                <td class="pt-3-half">
-                  <span class="table-up"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-up"
-                        aria-hidden="true"></i></a></span>
-                  <span class="table-down"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-down"
-                        aria-hidden="true"></i></a></span>
-                </td>
-                <td>
-                  <span class="table-remove"><button type="button"
-                      class="btn btn-danger btn-rounded btn-sm my-0">Remove</button></span>
-                </td>
-              </tr>
-             
-              <tr>
-                <td class="pt-3-half" contenteditable="true">Guadalupe House</td>
-                <td class="pt-3-half" contenteditable="true">26</td>
-                <td class="pt-3-half" contenteditable="true">Isotronic</td>
-                <td class="pt-3-half" contenteditable="true">Germany</td>
-                <td class="pt-3-half" contenteditable="true">Frankfurt am Main</td>
-                <td class="pt-3-half">
-                  <span class="table-up"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-up"
-                        aria-hidden="true"></i></a></span>
-                  <span class="table-down"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-down"
-                        aria-hidden="true"></i></a></span>
-                </td>
-                <td>
-                  <span class="table-remove"><button type="button"
-                      class="btn btn-danger btn-rounded btn-sm my-0">Remove</button></span>
-                </td>
-              </tr>
-             
-              <tr class="hide">
-                <td class="pt-3-half" contenteditable="true">Elisa Gallagher</td>
-                <td class="pt-3-half" contenteditable="true">31</td>
-                <td class="pt-3-half" contenteditable="true">Portica</td>
-                <td class="pt-3-half" contenteditable="true">United Kingdom</td>
-                <td class="pt-3-half" contenteditable="true">London</td>
-                <td class="pt-3-half">
-                  <span class="table-up"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-up"
-                        aria-hidden="true"></i></a></span>
-                  <span class="table-down"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-down"
-                        aria-hidden="true"></i></a></span>
-                </td>
-                <td>
-                  <span class="table-remove"><button type="button"
-                      class="btn btn-danger btn-rounded btn-sm my-0">Remove</button></span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+      
+      
 
 
       
      );
- }
-}
 
+
+ }
+} */
+
+
+export default function MaterialTableDemo() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'ID', field: 'id'},
+      { title: 'Name', field: 'name'},
+      { title: 'ABRV', field: 'abrv'},
+       { title: 'Production', field: 'year'},
+
+       
+     
+    ],
+    data: [
+      {  name: 'Opel Astra',
+         id: '03244',
+         abrv: 'AP-AS',
+         year: 2004
+
+    
+    },
+
+    {  
+      
+    name: 'Opel Astra',
+    id: '03244',
+    abrv: 'AP-AS',
+    year: 2004
+},
+
+
+{  
+      
+  name: 'Opel Astra',
+  id: '03244',
+  abrv: 'AP-AS',
+  year: 2004
+},
+
+
+{  
+      
+  name: 'Opel Astra',
+  id: '03244',
+  abrv: 'AP-AS',
+  year: 2004
+},
+
+{  
+      
+  name: 'Opel Astra',
+  id: '03244',
+  abrv: 'AP-AS',
+  year: 2004
+},
+
+      {
+        name: 'Mazda Asuno',
+        id: '34565',
+        abrv: 'MA-AS',
+        year: 2005
+      },
+    ],
+  });
+
+  return (
+    <MaterialTable
+      title="Editable Example"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState(prevState => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data }; 
+                });
+              }
+            }, 600);
+          }),
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+    />
+  );
+}

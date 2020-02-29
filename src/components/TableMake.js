bb11ad3e6ba5c3f884f3e4492e2c8c64d37a2149
Pero031 <p.cuckovic89@gmail.com>
2020-02-29 01:34:33 +0100

    Creating a new react table V6 - filtering, sorting, delete row

--- src/components/TableMake.js
+++ src/components/TableMake.js
@@ -1,84 +1,162 @@
 import React, {Component} from 'react';
 import ReactTable from "react-table-6"; 
 import '../layouts/Tables.css';
-
 /*import VehicleMakesStore from "../stores/VehicleMakes"; */
-
 import 'react-table-6/react-table.css';
 
 
 class TableMake extends Component {  
 
 
+      constructor(props){
+        super(props);
+
+        this.state = {
+             posts: []
+
+        }
+
+      }
+
+        componentDidMount(){
+            const url = "https://jsonplaceholder.typicode.com/posts";
+            fetch(url,{ method: "GET"
+          }).then(reponse => reponse.json()).then(posts => {
+              this.setState({posts: posts})
+          })
+        }
+   
+      
+      deleteRow(id) {
+          console.log("id", id)
+          const index = this.state.posts.findIndex(post =>{
+          return post.id === id 
+        })
+          console.log("index", index)
+          this.setState({
+            posts: [...this.state.posts.filter(post => post.id !== id)]
+            })
+      
+      }
     
-    render() {  
+
+/*primjerr za dodavanje novoga reda  */
+      addRow=(id)=> {
+       /* let dataArray = this.state.posts;
+        dataArray.push(id);
+        this.setState({data: dataArray});*/
+        this.setState({
+          posts: [...this.state.posts.filter(post => post.id !== id)]
+          })
+
+     }
+      
+      
+
+         
+       
+      render() {
+        const columns = [
+          {
+            Header:"User ID",
+            accessor:"userId"
+         },
+          {
+            Header:"ID",
+            accessor:"Id",
+            sortable: false,
+            filterable: false
+          },
+
+          {
+            Header:"Title",
+            accessor:"title",
+            sortable: false,
+            filterable: false
+          },
 
 
+          {
+            Header:"Content",
+            accessor:"body",
+            sortable: false,
+            filterable: false
+          },
 
-        
-       const data = [{  
-          name: 'Ayaan',  
-          age: 26  
-          },{  
-           name: 'Ahana',  
-           age: 22  
-           },{  
-           name: 'Peter',  
-           age: 40      
-           },{  
-           name: 'Virat',  
-           age: 30  
-           },{  
-           name: 'Rohit',  
-           age: 32  
-           },{  
-           name: 'Dhoni',  
-           age: 37  
-           }]  
-     const columns = [{  
-         Header: 'Name',  
-         accessor: 'name'  
-         },{  
-         Header: 'Age',  
-         accessor: 'age'  
-         }]  
-
-         remove = (rowId) => {
-            // Array.prototype.filter returns new array
-            // so we aren't mutating state here
-            const arrayCopy = this.state.data.filter((row) => row.id !== rowId);
-            this.setState({data: arrayCopy});
-          };
-<Row remove={this.remove} {...rowData } />);
-
-      return (  
+          {
+            Header: "Actions",
+            Cell: props => {
+              return(
 
 
-        
-            <div>  
+                <button style={{backgroundColor: "red", color:"#fefefe"}}
+                   onClick={() => {
+                   this.deleteRow(props.original.id);
+                  
+                }}>Delete</button>
 
 
+            
+   /*pokušati ovdje ubacii i ondaa kada izbaci error vidjeti što bi mogao biti problem   */
                 
-                <ReactTable  
-
-
-
-                    data={data}  
-                    columns={columns}  
-                    defaultPageSize = {2}  
-                    pageSizeOptions = {[2,4, 6]}  
+              )
+            },
 
 
 
 
-                    
-                />  
+            Cell: props => {
+              return(
 
+
+                <button style={{backgroundColor: "red", color:"#fefefe"}}
+                   onClick={() => {
+                   this.addRow(props.original.id);
+                  
+                }}>Add</button>
+
+
+            
+   /*pokušati ovdje ubacii i ondaa kada izbaci error vidjeti što bi mogao biti problem   */
                 
-            </div>        
-      )  
-    }  
-  }  
+              )
+            },
+
+
+
+
+
+
+
+
+            
+              sortable:false,
+              filterable: false,
+              width: 100,
+              minWidth: 100,
+              maxWidth: 100
+
+
+          }
+
+          ]
+        return (  
+          <ReactTable 
+            columns={columns}
+            data={this.state.posts} 
+            filterable
+            defaultPageSize = {10}
+            showPagination= {true}
+          >
+
+                                                                               
+
+
+          </ReactTable>
+          );
+        }
+    
+      }
   export default TableMake;  
 
 
